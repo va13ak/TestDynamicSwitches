0fada005a6da5764dab191d7399d7b9f5cba461b
va13ak <va13ak@gmail.com>
2018-10-23 01:34:40 +0300

    draw buttons dynamically

--- app/src/main/java/com/smartlab/testdynamicswitches/MainActivity.java
+++ app/src/main/java/com/smartlab/testdynamicswitches/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private HashMap<Enum<?>, Switch> switches = new HashMap<>();
+    private HashMap<View, String> buttons = new HashMap<>();
 
     int agentTagValue = 0;
     int taxCodeValue = 0;
@@ -123,27 +124,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         parent.addView(divider);
     }
 
+    public void drawButton(String name) {
+        drawButton(name, llMain);
+    }
+
+    public void drawButton(String name, ViewGroup parent) {
+        Button btn = new Button(this);
+        btn.setOnClickListener(this);
+        int stringByName = getResources().getIdentifier(name, "string", this.getPackageName());
+        btn.setText(getResources().getString(stringByName));
+
+        buttons.put(btn, name);
+
+        parent.addView(btn);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnRefresh = findViewById(R.id.btnRefresh);
-        btnRefresh.setOnClickListener(this);
-
-        btnRegistration = findViewById(R.id.btnRegistration);
-        btnRegistration.setOnClickListener(this);
-
-        btnReRegistration = findViewById(R.id.btnReRegistration);
-        btnReRegistration.setOnClickListener(this);
-
-        btnArchiveClosure = findViewById(R.id.btnArchiveClosure);
-        btnArchiveClosure.setOnClickListener(this);
-
-        firstDivider = findViewById(R.id.divider);
-
         llMain = findViewById(R.id.llMain);
 
+        drawButton("btnRefresh");
 
         drawDivider();
         drawTextView(getResources().getString(R.string.titleTaxCode));
@@ -160,23 +163,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         drawDivider();
         drawTextView(getResources().getString(R.string.titleAgentTag));
         drawSwitches(FiscalCoreEnums.AgentTag.values(), agentTagValue);
+
+        drawButton("btnRegistration");
+        drawButton("btnReRegistration");
+        drawButton("btnArchiveClosure");
     }
 
     @Override
     public void onClick(View view) {
-        int elementId = view.getId();
-        switch (elementId) {
-            case R.id.btnRefresh:
+        String buttonName = buttons.get(view);
+        if (buttonName == null) return;
+        switch (buttonName) {
+            case "btnRefresh":
                 showSwitches(FiscalCoreEnums.AgentTag.values());
                 break;
-            case R.id.btnRegistration:
+            case "btnRegistration":
                 registration();
                 break;
-            case R.id.btnReRegistration:
+            case "btnReRegistration":
                 showToast(getResources().getString(R.string.btnReRegistration));
                 registration();
                 break;
-            case R.id.btnArchiveClosure:
+            case "btnArchiveClosure":
                 showToast(getResources().getString(R.string.btnArchiveClosure));
                 break;
         }
